f58ac326d9ebf6cf968bf3636d561073e46550b4
F.Fitzgerald <mrffitzgerald@runbox.com>
2021-04-22 22:43:06 +0100

    start writing tests

--- test/test.py
+++ test/test.py
@@ -7,6 +7,9 @@ class TestSampleList(unittest.TestCase):
     def test_pass(self):
         self.assertEqual(1, 1)
 
+    def test_read_xrns(self):
+        sl.read_xrns("test/test.xrns")
+
 
 if __name__ == "__main__":
     unittest.main()
