8b77468bd7df15be05ad4566f080aed7db83c125
F.Fitzgerald <mrffitzgerald@runbox.com>
2021-08-15 21:05:22 +0100

    add setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,21 @@ import setuptools
 with open("README.md",'r') as fh:
     long_description = fh.read()
 
-setuptools.setup()
+setuptools.setup(
+        name='',
+        version='',
+        description='',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author='F Fitzgerald',
+        url='https://github.com/mrffr/xrns-sample-list',
+
+        entry_points={
+            'console_scripts': [
+                'xrns-sample-list = xrns-sample-list.sample_list:main',
+                ],
+            },
+
+        packages=['xrns-sample-list'],
+        classifie
+        )
